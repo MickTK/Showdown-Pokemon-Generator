07417604346bbc5ef02faddea7b0544bec9a6bed
MickTK <63038410+MickTK@users.noreply.github.com>
2021-09-09 22:06:43 +0200

    Update

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ class Pokemon{
         this.id = undefined;
         this.name = "";
 
-        this.hp = undefined;
-        this.atk = undefined;
-        this.def = undefined;
-        this.spa = undefined;
-        this.spd = undefined;
-        this.spe = undefined;
+        this.hp = 0;
+        this.atk = 0;
+        this.def = 0;
+        this.spa = 0;
+        this.spd = 0;
+        this.spe = 0;
 
         this.ability = undefined;
         this.shiny = false;
@@ -25,9 +25,8 @@ class Pokemon{
     build(){
         if(this.name == "") return undefined;
         
-        var a;
-
         // Core
+        var a, b;
         $.ajax({ 
             type: 'GET',
             url: 'https://pokeapi.co/api/v2/pokemon/' + this.name,
@@ -45,14 +44,51 @@ class Pokemon{
         this.ability = this.link.abilities[randomNum(0, this.link.abilities.length)].ability.name;
 
         // Nature
-        
+        $.ajax({ 
+            type: 'GET',
+            url: "https://pokeapi.co/api/v2/nature?offset=" + randomNum(0, 25) + "&limit=1",
+            async: false,
+            data: {},
+            dataType: 'json',
+            success: function (data) {
+                a = data.results[0].name;
+            }
+        });
+        this.nature = a;
+
+        // EVs
+        a = randomNum(0, 6);
+        b = randomNum(0, 6);
+        while(a == b){
+            a = randomNum(0, 6);
+            b = randomNum(0, 6);
+        }
+        switch(a){
+            case 0: this.hp = 255; break;
+            case 1: this.atk = 255; break;
+            case 2: this.def = 255; break;
+            case 3: this.spa = 255; break;
+            case 4: this.spd = 255; break;
+            case 5: this.spe = 255; break;
+        }
+        switch(b){
+            case 0: this.hp = 255; break;
+            case 1: this.atk = 255; break;
+            case 2: this.def = 255; break;
+            case 3: this.spa = 255; break;
+            case 4: this.spd = 255; break;
+            case 5: this.spe = 255; break;
+        }
 
         // Moves
         var b;
-        console.log(this.link);
-        b = this.link.moves[randomNum(0, this.link.moves.length)].move.name;
-        this.moves.push(b);
-        while(this.moves.length < 4){
+        if(!this.link.moves.length == 0){
+            b = this.link.moves[randomNum(0, this.link.moves.length)].move.name;
+            this.moves.push(b);
+        }
+        else
+            this.moves.push("pound");
+        while(this.moves.length < 4 && this.moves.length < this.link.moves.length){
             b = this.link.moves[randomNum(0, this.link.moves.length)].move.name;
             if(!this.moves.includes(b))
             this.moves.push(b);
@@ -63,23 +99,27 @@ class Pokemon{
         var result;
 
         // Name
-        result = this.name + "\n";
+        result = capitalizeFirstLetter(this.name) + "\n";
 
         // EVs
-        result += "EVs: " + this.hp[0] + " HP / " + this.atk[1] + " Atk / " + this.def[2] + " Def / " + this.spa[3] + " SpA / " + this.spd[4] + " SpD / " + this.spe[5] + " Spe\n";
+        result += "EVs: " + this.hp + " HP / " + this.atk + " Atk / " + this.def + " Def / " + this.spa + " SpA / " + this.spd + " SpD / " + this.spe + " Spe\n";
+
+        // Ability
+        result += "Ability: " + capitalizeFirstLetter(this.ability.replace('-', ' ')) + "\n";
 
         // Shiny
         if(this.shiny)
             result += "Shiny: Yes\n";
 
-        // Ability
-        result += this.ability + " Nature\n";
+        // Nature
+        result += capitalizeFirstLetter(this.nature) + " Nature\n";
 
+        var x, y;
         // Moves
-        result += "- " + this.moves[0] + "\n";
-        result += "- " + this.moves[1] + "\n";
-        result += "- " + this.moves[2] + "\n";
-        result += "- " + this.moves[3] + "\n";
+        for(var i = 0; i < this.moves.length; i++){
+            x = this.moves[i].replace('-', ' ');
+            result += "- " + capitalizeFirstLetter(x) + "\n";
+        }
 
         return result;
     }
@@ -96,16 +136,30 @@ function generate(){
     pokemon.link = retrieve_pokemon(pokemon.id);
     research_name = pokemon.link.name.replace('-', '');
     pokemon.name = pokemon.link.name;
+
+    if(randomNum(0, 10) == 0){
+        pokemon.shiny = true;
+        $("#name").text(pokemon.link.name.toUpperCase() + " *");
+    }
+    else
     $("#name").text(pokemon.link.name.toUpperCase());
-    $('#img').attr("src","https://play.pokemonshowdown.com/sprites/ani/" + research_name + ".gif");
+
+    if(pokemon.shiny)
+        $('#img').attr("src","https://play.pokemonshowdown.com/sprites/ani-shiny/" + research_name + ".gif");
+    else
+        $('#img').attr("src","https://play.pokemonshowdown.com/sprites/ani/" + research_name + ".gif");
     
     pokemon.build();
-
-    console.log(pokemon);
+    
+    $("#pokemon-stats").val(pokemon.make());
 }
 
 function randomNum(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function retrieve_pokemon(id){
